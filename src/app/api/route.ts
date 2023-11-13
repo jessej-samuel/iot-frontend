942679371ad7fb0a6a4d5a269299b3c273df2b77
Jessej Samuel <1234jessej@gmail.com>
2023-11-13 13:33:05 +0530

    delete: unnecessary API route function.

--- src/app/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { database } from "@/constants";
-import { get, ref } from "firebase/database";
-
-export async function GET({}: { res: Response }) {
-  // res.headers.set("Content-Type", "application/json");
-  const snapshot = await get(ref(database, "/"));
-  const data = snapshot.val();
-  return Response.json(data);
-}
